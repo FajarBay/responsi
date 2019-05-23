489d19be0e3ceaaa7cd9247f0e7989a9858007c4
fajarbay <fajarbay@github.com>
2019-05-23 09:58:36 +0700

    Updates

---
+++ precache-manifest.5c9ee09ec683032a5c814f7c0810a30b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae26a594de0491cb592d3ff2bd7738b6",
+    "url": "/responsi/index.html"
+  },
+  {
+    "revision": "9994e661ed2ab95dc1df",
+    "url": "/responsi/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsi/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9994e661ed2ab95dc1df",
+    "url": "/responsi/static/js/main.c7fd8a7f.chunk.js"
+  },
+  {
+    "revision": "d5ba698fb88a08d77ebc",
+    "url": "/responsi/static/js/runtime~main.a23198ed.js"
+  }
+]);
