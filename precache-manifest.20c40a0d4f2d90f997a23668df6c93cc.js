7b6d2a5cb4c1552754dd06f8df753d022fb617cb
fajarbay <fajarbay@github.com>
2019-05-23 09:51:10 +0700

    Updates

---
+++ precache-manifest.20c40a0d4f2d90f997a23668df6c93cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c7a53ac83cea20a4990aa6ee9097e21",
+    "url": "/responsiweb3/index.html"
+  },
+  {
+    "revision": "a33a0aa031beaac377a9",
+    "url": "/responsiweb3/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsiweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a33a0aa031beaac377a9",
+    "url": "/responsiweb3/static/js/main.59255000.chunk.js"
+  },
+  {
+    "revision": "b3cf34c6c7e486bae153",
+    "url": "/responsiweb3/static/js/runtime~main.cf154cc8.js"
+  }
+]);
